3716b243b7db056cc6696dd1aeaf8471fe9e77bf
Тимур Жигмытов <timur.zhigmytov@innoforce.kz>
2019-08-09 13:07:17 +0600

    Add simple cache feature

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,51 @@
-self.addEventListener("install", () => {
-    console.log("[Service Worker] Service worker install!");
+var CACHE_STATIC_NAME = 'static-v4';
+//var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+self.addEventListener("install", (event) => {
+    // console.log("[Service Worker] Service worker install!");
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(function(cache) {
+            console.log('[Service Worker] Precaching App Shell');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ]);
+        })
+    );
 });
 
 self.addEventListener("activate", (event) => {
-    console.log("[Service Worker] Service worker activate!", event);
-    return self.clients.claim();
+    // console.log("[Service Worker] Service worker activate!", event);
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.open(CACHE_STATIC_NAME).then(cache => {
+            return cache.match(event.request)
+                .then((response) => {
+                    if (response) {
+                        console.log('[Service worker] Response from Cache', event.request.url);
+                        return response;
+                    } else {
+                    return fetch(event.request)
+                        .then((res) => {
+                            console.log('[Service worker] Fetch Response', event.request.url);
+                            cache.put(event.request.url, res.clone());
+                            return res;
+                        })
+                        .catch(function(err) {});
+                    }
+                })
+        })
+    );
 });
